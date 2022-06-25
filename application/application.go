4e68596cb3ab0e39cd82e8a5598d0dafb08b50fd
evertonesc <evertonsouzaesc@gmail.com>
2022-06-25 11:50:09 -0300

    feat: init application setup

--- application/application.go
+++ application/application.go
@@ -2,3 +2,13 @@ package application
 
 type Engine struct {
 }
+
+func New() *Engine {
+	engine := &Engine{}
+	engine.loadDependencies()
+	return engine
+}
+
+func (e *Engine) loadDependencies() {
+
+}
