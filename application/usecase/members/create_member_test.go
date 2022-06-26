5249b1a9be2c4ff835422a973c4a9bcd54f42104
evertonesc <evertonsouzaesc@gmail.com>
2022-06-26 00:05:30 -0300

    test: add tests for use case (#1)

--- application/usecase/members/create_member_test.go
+++ application/usecase/members/create_member_test.go
@@ -2,6 +2,9 @@ package usecase
 
 import (
 	"context"
+	"errors"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 	"orinz/application/adapter/rest/presenter"
 	"orinz/application/domain/member"
 	"testing"
@@ -19,17 +22,44 @@ func TestCreateMemberUseCase_Execute(t *testing.T) {
 		name    string
 		fields  fields
 		args    args
-		wantErr bool
+		wantErr error
 	}{
-		// TODO: Add test cases.
+		{
+			name: "should return error when repository fails to insert a member",
+			fields: fields{
+				repository: func() member.Repository {
+					mock := member.NewMockRepository(gomock.NewController(t))
+					mock.EXPECT().
+						CreateMember(gomock.Any(), member.Member{
+							Name:      "Clive Staples Lewis",
+							BirthDate: "1989-11-29",
+							Address:   "Heaven street",
+							Email:     "cslewis@narnia.com",
+						}).
+						Return(errors.New("error when trying to create a member"))
+					return mock
+				}(),
+			},
+			args: args{
+				ctx: context.Background(),
+				memberRequest: presenter.MemberRequest{
+					Name:      "Clive Staples Lewis",
+					BirthDate: "1989-11-29",
+					Address:   "Heaven street",
+					Email:     "cslewis@narnia.com",
+				},
+			},
+			wantErr: errors.New("error when trying to create a member"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			uc := CreateMemberUseCase{
-				repository: tt.fields.repository,
-			}
-			if err := uc.Execute(tt.args.ctx, tt.args.memberRequest); (err != nil) != tt.wantErr {
-				t.Errorf("Execute() error = %v, wantErr %v", err, tt.wantErr)
+			uc := NewCreateMemberUseCase(tt.fields.repository)
+			err := uc.Execute(tt.args.ctx, tt.args.memberRequest)
+			if err != nil {
+				assert.Equalf(t, tt.wantErr, err, "Execute() error = %v, wantErr %v", err, tt.wantErr)
+			} else {
+				assert.Nil(t, err)
 			}
 		})
 	}
