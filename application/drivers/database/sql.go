bcfef2d34299c84589b3907083aeadeecd978b50
evertonesc <evertonsouzaesc@gmail.com>
2022-07-09 16:03:18 -0300

    feat: establish database connection (#2)

--- application/drivers/database/sql.go
+++ application/drivers/database/sql.go
@@ -1,7 +1,31 @@
 package database
 
-import "context"
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
 
-type SqlDatabase interface {
-	ConnectSQL(ctx context.Context, databaseURL string) (any, error)
+const templateDNS = "%s:%s@tcp(%s)/%s"
+
+// ConnectSQL uses gorm to provide database connection.
+// The dns template used is 'username:password@tcp(address)/dbname'
+func ConnectSQL() *gorm.DB {
+
+	DNS := fmt.Sprintf(templateDNS, os.Getenv("MARIADB_USER"), os.Getenv("MARIADB_PASS"), os.Getenv("MARIADB_HOST"), os.Getenv("MARIADB_DATABASE"))
+
+	db, err := gorm.Open(mysql.Open(DNS))
+	if err != nil {
+		log.Fatalf("database initialization fail - error: %s", err.Error())
+	}
+
+	sqlDb, err := db.DB()
+	if err != nil {
+		log.Fatalf("fail when trying to get master connection - error: %s", err.Error())
+	}
+	defer sqlDb.Close()
+
+	return db
 }
