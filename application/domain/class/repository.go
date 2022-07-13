02be3f5ec2324fd1c344ec81f83d642e3faabddd
evertonesc <evertonsouzaesc@gmail.com>
2022-07-13 19:23:12 -0300

    feat: add class repository interface (#5)

---
+++ application/domain/class/repository.go
@@ -0,0 +1,9 @@
+package class
+
+//go:generate mockgen -source=$GOFILE -destination=mock_$GOFILE -package=$GOPACKAGE
+
+import "context"
+
+type Repository interface {
+	Create(ctx context.Context, class Class) error
+}
