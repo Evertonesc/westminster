a706fff581937b096d87bbc1aba5f70c07414f62
esc <evertonsouzaesc@gmail.com>
2022-11-15 12:31:38 -0300

    refactor: change create member interface return signature (#1)

--- application/adapter/rest/handler/member.go
+++ application/adapter/rest/handler/member.go
@@ -3,8 +3,9 @@ package handler
 import (
 	"context"
 	"orinz/application/adapter/rest/presenter"
+	"orinz/application/domain/member"
 )
 
 type CreateMemberUseCase interface {
-	Execute(ctx context.Context, mr presenter.MemberRequest) error
+	Execute(ctx context.Context, mr presenter.MemberRequest) (member.Member, error)
 }
