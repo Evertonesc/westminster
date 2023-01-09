d5da1c6c43c94278ecbd8019c0bde35575a7d8fd
esc <evertonsouzaesc@gmail.com>
2023-01-09 19:59:56 -0300

    refactor: remove repository.go

--- application/domain/member/repository.go
+++
@@ -1,9 +0,0 @@
-package member
-
-//go:generate mockgen -source=$GOFILE -destination=mock_$GOFILE -package=$GOPACKAGE
-
-import "context"
-
-type Writer interface {
-	CreateMember(ctx context.Context, member Member) (Member, error)
-}
