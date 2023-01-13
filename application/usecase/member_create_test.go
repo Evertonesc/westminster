ca45831c1739f68faf10c32abfe53d3093bd3238
esc <evertonsouzaesc@gmail.com>
2023-01-13 20:40:56 -0300

    test: update unit tests

--- application/usecase/member_create_test.go
+++ application/usecase/member_create_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 	"westminster/application/adapter/rest/presenter"
 	"westminster/application/domain"
+	"westminster/application/domain/mocks"
 )
 
 func TestCreateMemberUseCase_Execute(t *testing.T) {
@@ -33,7 +34,7 @@ func TestCreateMemberUseCase_Execute(t *testing.T) {
 			name: "should successfully create a member",
 			fields: fields{
 				w: func() domain.MemberWriter {
-					mock := domain.NewMockMemberWriter(ctrl)
+					mock := mocks.NewMockMemberWriter(ctrl)
 					mock.EXPECT().
 						CreateMember(gomock.Any(), domain.Member{
 							Name:            "some name",
@@ -77,7 +78,7 @@ func TestCreateMemberUseCase_Execute(t *testing.T) {
 			name: "should return an error when the database writer fails",
 			fields: fields{
 				w: func() domain.MemberWriter {
-					mock := domain.NewMockMemberWriter(ctrl)
+					mock := mocks.NewMockMemberWriter(ctrl)
 					mock.EXPECT().
 						CreateMember(gomock.Any(), domain.Member{
 							Name:            "some name",
