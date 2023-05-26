d0cb1886a60883a93b2920c2e347818b39bc9099
esc <evertonsouzaesc@gmail.com>
2023-05-26 16:10:31 -0300

    add initial structure of the handler

--- application/adapter/rest/handler/member.go
+++ application/adapter/rest/handler/member.go
@@ -1,11 +1,24 @@
 package handler
 
+//go:generate mockgen -source=$GOFILE -destination=mock_$GOFILE -package=$GOPACKAGE
+
 import (
 	"context"
+
 	"westminster/application/domain"
 	"westminster/application/usecase"
 )
 
 type CreateMemberUseCase interface {
 	Execute(ctx context.Context, i usecase.MemberInteractor) (domain.Member, error)
+}
+
+type MemberHandler struct {
+	uc CreateMemberUseCase
+}
+
+func NewMemberHandler(uc CreateMemberUseCase) MemberHandler {
+	return MemberHandler{
+		uc: uc,
+	}
 }
