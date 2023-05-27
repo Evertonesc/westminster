cf5c202999939a73367bf8f561c4d1316b4128f0
esc <evertonsouzaesc@gmail.com>
2023-05-27 18:06:14 -0300

    feat: start the API with chi router (#8)

--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
 	"westminster/application"
+	"westminster/application/adapter/rest/handler"
 )
 
 func main() {
 	app := application.New()
 	fmt.Println(app)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome from chi rounter!"))
+	})
+
+	memberHandler := handler.NewMemberHandler(nil)
+
+	r.Post("/members", memberHandler.CreateMember)
+	http.ListenAndServe(":3000", r)
 }
