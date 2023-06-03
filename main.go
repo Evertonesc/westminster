8992759a448810880f7480c95650ce916008e1a2
esc <evertonsouzaesc@gmail.com>
2023-06-03 14:34:24 -0300

    add the routes register to api startup (#8)

--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
 	"westminster/application"
+	"westminster/application/adapter/rest"
 	"westminster/application/adapter/rest/handler"
 )
 
 func main() {
-	app := application.New()
-	fmt.Println(app)
+	_ = application.New()
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome from chi rounter!"))
-	})
 
 	memberHandler := handler.NewMemberHandler(nil)
+
+	rest.RegisterRoutes(r, memberHandler)
 
 	r.Post("/members", memberHandler.CreateMember)
 	http.ListenAndServe(":3000", r)
