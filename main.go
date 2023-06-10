c46572baca46dfd0adba46ecbda27c8fea41f228
esc <evertonsouzaesc@gmail.com>
2023-06-10 15:39:33 -0300

    refactor: start the HTTP server in the main (#11)

--- main.go
+++ main.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-
 	"westminster/application"
-	"westminster/application/adapter/rest"
-	"westminster/application/adapter/rest/handler"
 )
 
 func main() {
-	_ = application.New()
-
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-
-	memberHandler := handler.NewMemberHandler(nil)
-
-	rest.RegisterRoutes(r, memberHandler)
-
-	r.Post("/members", memberHandler.CreateMember)
-	http.ListenAndServe(":3000", r)
+	appEngine := application.New()
+	appEngine.StartHTTPServer()
 }
